84e2a0a06e0505f82e466e7cbbae96dd2cb4afe9
bruno-santos07 <limasantosb8@gmail.com>
2026-02-09 17:11:04 -0300

    Aula 011 adicionada

---
+++ Aula011/ex008.js
@@ -0,0 +1,8 @@
+let vel = 60.5
+console.log('A velocidade do seu carro: ' + vel)
+
+if(vel >= 80){
+    console.log('Você ultrapassou da velocidade permitida!')
+}else{
+    console.log('Vc está dentro da velocidade permitida! Dirija sempre usando cinto de segurança!')
+}
---
+++ Aula011/ex009.js
@@ -0,0 +1,14 @@
+let paiss = 'Brasil'
+
+if(paiss == 'Brasil'){
+    console.log('Você é Brasileira!')
+}else{
+    console.log('Você é Estrangeiro!')
+}
+
+//let pais = 'EUA'
+
+//if(pais != 'Brasil'){
+//    console.log('Você é Brasileira!')
+// }else{
+//    console.log('Você é Estrangeiro!')}
