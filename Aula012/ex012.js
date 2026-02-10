998a68dc6482bbe6d3d1246dcbf542171d0d1f72
bruno-santos07 <limasantosb8@gmail.com>
2026-02-10 18:02:49 -0300

    Atualizado

---
+++ Aula012/ex012.js
@@ -0,0 +1,11 @@
+let agora = new Date()
+let hora = agora.getHours
+console.log(`Agora são exatamente ${hora} horas.`)
+
+if(hora < 12){
+    console.log(`Olá Bom dia!`)
+}else if(hora <= 18){
+    console.log('Boa tarde!')
+}else{
+    console.log('Boa noite!')
+}
